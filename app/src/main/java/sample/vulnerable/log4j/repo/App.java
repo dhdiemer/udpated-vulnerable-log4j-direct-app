cdeab6ae4f796583fbc51f4636ec4192a025dd93
Quinn Slack <quinn@slack.org>
2021-12-12 21:33:05 -0800

    use log4j (vulnerable version)

--- app/src/main/java/sample/vulnerable/log4j/repo/App.java
+++ app/src/main/java/sample/vulnerable/log4j/repo/App.java
@@ -1,7 +1,12 @@
 package sample.vulnerable.log4j.repo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+    private static final Logger logger = LogManager.getLogger("App");
+	
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+		logger.error("Args: {}", (Object[])(args));
     }
 }
